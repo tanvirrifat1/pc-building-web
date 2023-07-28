f24e6ac73598985bcff20665ec3b3ab9d86d7754
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-07-29 00:37:36 +0600

    added more

--- src/pages/allPc/[allPcId].js
+++ src/pages/allPc/[allPcId].js
@@ -2,11 +2,33 @@ import React from "react";
 import RootLayout from "../layout/RootLayout";
 
 const AllPcDetails = ({ desktop }) => {
-  console.log(desktop);
   return (
-    <div>
-      <h1>{desktop.category}</h1>
-      <h1>here us:</h1>
+    <div className="mt-6">
+      <div className="card lg:card-side bg-base-300 shadow-xl">
+        <figure>
+          <img src={desktop.image} alt="Album" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Name:{desktop.category}</h2>
+          <div className="py-2 ">
+            <p className="font-semibold py-2">Model: {desktop.product_name}</p>
+            <p className="font-semibold py-2">status: {desktop.status}</p>
+            <p className="font-semibold py-2">price: {desktop.price}</p>
+            <p className="font-semibold py-2">
+              description: {desktop.description}
+            </p>
+            <p className="font-semibold py-2">
+              brand: {desktop.key_features.brand}
+            </p>
+            <p className="font-semibold py-2">
+              rating: {desktop.average_rating}
+            </p>
+          </div>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
