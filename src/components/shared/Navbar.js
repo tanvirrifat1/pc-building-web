2bc40c6a62d368e8808b94b7463d1c077e686d96
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-07-29 01:08:49 +0600

    complete login and signup design

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -26,9 +26,10 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Homepage</a>
-            </li>
+            <Link href="/login">
+              <button className="btn btn-outline text-black mr-3">Login</button>
+            </Link>
+
             <li>
               <a>Portfolio</a>
             </li>
@@ -46,25 +47,10 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-        <button className="btn btn-secondary btn-outline rounded-xl">
-          PC Build
-        </button>
+        {/* <Link href="/login">
+          <button className="btn btn-outline text-black mr-3">Login</button>
+        </Link> */}
+        <button className="btn btn-outline text-black">PC Build</button>
       </div>
     </div>
   );
